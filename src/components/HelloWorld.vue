86890ca970111fd87ec6be2e870f78bafc918cf9
tesso <ta4780@icloud.com>
2020-07-02 01:24:49 +0900

    完成？？？

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,18 +2,24 @@
   <div>
     <h1>{{ msg }}</h1>
     <counter />
+    <ItemList />
+    <!--counterを持ってくる -->
   </div>
 </template>
 
 <script>
-import Counter from "./Counter.vue";
+import Counter from "./Counter.vue"; //componentを読み込む
+import ItemList from "./ItemList";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
   },
   components: {
-    Counter
+    //componentの登録
+    Counter,
+    ItemList
   }
 };
 </script>
---
+++ src/components/ItemList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <div>ItemList</div>
+    <div v-for="item in items" :key="item.name">
+      <div class="item" :class="{ over500: item.price >= 500 }">
+        <div class="name">名前：{{ item.name }}</div>
+        <div class="price">{{ item.price }}</div>
+        <div v-if="item.price >= 10000">高額商品</div>
+      </div>
+    </div>
+    <div>
+      <label>
+        名前
+        <input type="text" v-model="newItemName" />
+      </label>
+      <label>
+        価格
+        <input type="number" v-model="newItemPrice" />
+      </label>
+      <button @click="addItem">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ItemList.vue",
+  data() {
+    //基本的に変数はdata関数の中身に記述
+    return {
+      items: [
+        { name: "たまご", price: 100 },
+        { name: "りんご", price: 160 }
+      ]
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.newItemPrice != null && this.newItemName != null) {
+        this.items.push({ name: this.newItemName, price: this.newItemPrice });
+        this.newItemName = null;
+        this.newItemPrice = null;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+.over500 {
+  color: red;
+}
+</style>
