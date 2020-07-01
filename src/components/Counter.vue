8fbdc3477eb7fc2ac4e6999fe613360ff8019536
tesso <ta4780@icloud.com>
2020-07-02 00:58:23 +0900

    change Counter.vue

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,9 +1,25 @@
-<template> </template>
+<template>
+  <div>
+    <div>{{ countMessage }}</div>
+    <button @click="count++">クリック！</button>
+    <button @click="count = 0">リセット！</button>
+  </div>
+</template>
 
 <script>
 export default {
-  name: "Counter"
+  name: "counter",
+  data() {
+    return {
+      count: 0
+    };
+  },
+  computed: {
+    countMessage() {
+      return "回数: " + this.count;
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style></style>
